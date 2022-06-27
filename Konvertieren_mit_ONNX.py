bcaba2e72b14804e2725863066e1b462bda87671
Philipp C <philipp.christ@online.de>
2022-06-27 15:10:04 +0200

    onnx ist nix

--- Konvertieren_mit_ONNX.py
+++ Konvertieren_mit_ONNX.py
@@ -1,16 +1,22 @@
+from importlib import import_module
 from tensorflow.keras.applications.resnet50 import ResNet50
-import keras2onnx
+import tf2onnx
 import tensorflow as tf
 import numpy as np
+import onnx
 
 
 BATCH_SIZE = 32
 shape = (BATCH_SIZE, 224, 224, 3)
 
+input_signature = [tf.TensorSpec([shape], tf.float32, name='x')]
+
 # load keras model
 model = ResNet50(include_top=True, weights='imagenet')
 
 
-#output_model_path = "keras_efficientNet.onnx"
-#onnx_model = keras2onnx.convert_keras(model, model.name)
-#keras2onnx.save_model(onnx_model, output_model_path)
+output_model_path = "keras_efficientNet.onnx"
+# convert model to ONNX
+onnx_model = tf2onnx.convert.from_keras(model, input_signature=input_signature)
+
+onnx.save_model(onnx_model, "example.onnx")
